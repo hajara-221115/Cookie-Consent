5353f9634e0877b88ebb3f3a1de6101827049ee3
Hajarabee Shaik <n221115@rguktn.ac.in>
2026-06-21 21:31:35 +0530

    Fix modal thank you message formatting

--- dist/assets/index-BR_SfG6b.js
+++ dist/assets/index-BR_SfG6b.js
@@ -4,7 +4,7 @@
         <p id="upload-text">Uploading your data to the dark web...</p>
     </div>`,setTimeout(function(){document.getElementById(`upload-text`).innerText=`
         Making the sale...`},1500),setTimeout(function(){document.getElementById(`modal-inner`).innerHTML=`
-        <h2>Thanks <span class="modal-display-name">${i}</span>, you sucker! </h2>
+        <h2>Thanks <span class="modal-display-name">${i}</span> </h2>
         <p>We just sold the rights to your eternal soul.</p>
         <div class="idiot-gif">
             <img src="images/pirate.jpg">
